89912e391f2ac24ae3bf7fbafd1c1cf03336a06b
Sushil Kumar <sushilkumbhar77@gmail.com>
2026-07-06 22:43:53 +0530

    update

--- views/welcome.php
+++ views/welcome.php
@@ -2,6 +2,9 @@
 addPage('include/header.php');
 ?>
 <h3>Welcome to project <span>Auth</span>.</h3>
+<form>
+<input type="search" name="s" placeholder="Search..."/>
+</form>
 <?php
 addPage('include/footer.php');
 ?>
