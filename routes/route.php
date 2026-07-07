28face8e3aa9b73825f53f7a035603cc5b1334d5
Sushil Kumar <sushilkumbhar77@gmail.com>
2026-07-07 17:47:29 +0530

    add auth route

--- routes/route.php
+++ routes/route.php
@@ -4,4 +4,6 @@ use App\Router as Route;
 //use App\Middleware\Auth;
 //use App\Middleware\Guest;
 
-Route::get('','HomeController','index');
+Route::get('', 'HomeController', 'index');
+
+Route::get('login', 'AuthController', 'index');
