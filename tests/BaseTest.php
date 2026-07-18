a1affe60e416824a2fa24941cc4c2c16c35f48ce
Sushil Kumar <sushilkumbhar77@gmail.com>
2026-07-18 10:28:58 +0530

    add tests

---
+++ tests/BaseTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseTest extends TestCase
+{
+    public function test_add_two_num(){
+        $this->assertSame(4, 4);
+    }
+}
